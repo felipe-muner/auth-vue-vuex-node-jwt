1186558101a0dfe7a470d35f6d0129fa49bcd87e
felipemuner <felipe.muner@gmail.com>
2020-04-24 18:32:46 +0700

    set up date field last version

--- client/src/components/DateOfBirth.vue
+++ client/src/components/DateOfBirth.vue
@@ -1,35 +1,12 @@
-<template>
-  <v-menu
-    v-model="menuDate"
-    :close-on-content-click="false"
-    transition="scale-transition"
-    offset-y
-    max-width="290px"
-    min-width="290px"
-  >
-    <template v-slot:activator="{ on }">
-      <v-text-field
-        outlined
-        v-model="computedDateFormatted"
-        label="Date of Birth"
-        hint="DD/MM/YYYY format"
-        persistent-hint
-        prepend-icon="event"
-        readonly
-        v-on="on"
-      ></v-text-field>
-    </template>
-    <v-date-picker v-model="date" no-title @input="menuDate = false"></v-date-picker>
-  </v-menu>
-</template>
+<template></template>
 
 <script>
 export default {
-  data: vm => ({
-    date: "",
-    dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
-    menuDate: false
-  }),
+  data() {
+    return {
+      menuDate: false
+    };
+  },
 
   computed: {
     computedDateFormatted() {
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -36,7 +36,28 @@
                   prepend-icon="person"
                   type="text"
                 />
-                <DateOfBirth />
+                <v-menu
+                  v-model="menuDate"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  offset-y
+                  max-width="290px"
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      outlined
+                      v-model="computedDateFormatted"
+                      label="Date of Birth"
+                      hint="DD/MM/YYYY format"
+                      persistent-hint
+                      prepend-icon="event"
+                      readonly
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="date" no-title @input="menuDate = false"></v-date-picker>
+                </v-menu>
               </v-card-text>
               <v-card-actions>
                 <v-spacer />
@@ -51,13 +72,12 @@
 </template>
 
 <script>
-import DateOfBirth from "./DateOfBirth";
 export default {
-  components: {
-    DateOfBirth
-  },
   data() {
     return {
+      date: "",
+      dateFormatted: "",
+      menuDate: false,
       valid: true,
       name: "",
       email: "",
@@ -70,7 +90,16 @@ export default {
       nameRules: [v => !!v || "Name is required"]
     };
   },
-  watch: {},
+  watch: {
+    date(val) {
+      this.dateFormatted = this.formatDate(this.date);
+    }
+  },
+  computed: {
+    computedDateFormatted() {
+      return this.formatDate(this.date);
+    }
+  },
   methods: {
     register() {
       if (this.$refs.form.validate()) {
@@ -78,6 +107,18 @@ export default {
       } else {
         alert("nao validei");
       }
+    },
+    formatDate(date) {
+      if (!date) return null;
+
+      const [year, month, day] = date.split("-");
+      return `${day}/${month}/${year}`;
+    },
+    parseDate(date) {
+      if (!date) return null;
+
+      const [month, day, year] = date.split("/");
+      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
     }
   }
 };
